385f69f8669cbdb5981658e4911c6ceba43a8982
MEGBLETO <carnelilordwiz@gmail.com>
2022-07-15 17:17:01 +0200

    cors error

--- back/src/routes/dataroute.js
+++ back/src/routes/dataroute.js
@@ -13,4 +13,23 @@ router.get('/citydata', async(req, res) => {
   }
 });
 
+
+
+
+router.get('/citydata/meteodata/:name', async(req, res) => {
+
+    let city = req.params.name;
+
+    let query = `select details where name=${city} FROM (select  city.insee, forecast.details, city.name from city INNER JOIN forecast ON city.insee=forecast.insee)`;
+
+  
+    try {
+      db.all(query).then((rows) => {
+          res.send(rows)
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
 module.exports = router;
--- front/src/components/City.js
+++ front/src/components/City.js
@@ -3,21 +3,47 @@ import axios from 'axios';
 
 const City = () => {
   const [citydata, setCitydata] = useState([]);
+  const [cityinsee, setcityinsee] = useState('');
 
   const getCities = async () => {
     const req = await axios.get('http://localhost:5000/api/citydata');
     const res = await req.data;
     setCitydata(res);
+  };
+
+  const getInsee = (id) => {
+    setcityinsee(id);
+  };
+
+  const config = {
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+    },
+  };
+
+
+  const getWeatherforecast = async() =>{
+    const req = await axios.get(`https://candidat.adcleek.it/cities/${cityinsee}/forecast`,config );
+    const res = await req.data;
 
     console.log(res);
-  };
+  }
+
+
+
+  useEffect(()=>{
+    if(cityinsee){
+       getWeatherforecast(cityinsee)
+    }
+  
+  },[cityinsee])
 
   useEffect(() => {
     getCities();
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-300">
+    <div className="min-h-screen flex bg-gray-300 w-1/2">
       {citydata ? (
         <table className="table-auto h-screen w-full ">
           <thead>
@@ -28,7 +54,12 @@ const City = () => {
             </tr>
           </thead>
           {citydata.map((item) => (
-            <tbody className="h-30">
+            <tbody
+              key={item.id}
+              id={item.insee}
+              onClick={(e) => getInsee(e.currentTarget.id)}
+              className="h-30 cursor-pointer hover:bg-orange-400"
+            >
               <tr className="w-ful p-4 border-2 my-4">
                 <td>{item.insee}</td>
                 <td>{item.name}</td>
@@ -40,6 +71,11 @@ const City = () => {
       ) : (
         <>Loading.......</>
       )}
+
+
+      <div className='flex-1'>
+
+      </div>
     </div>
   );
 };
